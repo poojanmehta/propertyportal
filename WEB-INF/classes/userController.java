b019307676f56a7018f9fbc1774ade56632c66de
poojanmehta <poojanmehta2000@gmail.com>
2021-12-18 00:05:21 +0530

    first commit of property branch

--- WEB-INF/classes/userController.java
+++ WEB-INF/classes/userController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class userController extends HttpServlet {
+    private static final String USER = "user";
+    private static final String USERREG = "userregister";
+    private static final String USERLOG = "userlogin";
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         try {
--- property.jsp
+++ property.jsp
@@ -1,4 +1,4 @@
 <h2>Enter Your Roll no to view Marksheet</h2>
 <form action="user" method="post">
-    <input type="submit" value="submit" /> 
+    <input type="submit" value="submit" />
 </form>
